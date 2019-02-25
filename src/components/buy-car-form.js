93f365ac1a06fa1badf561446a813653c589df7d
Medhat Dawoud <medhatdawoud@gmail.com>
2019-02-25 04:11:16 +0100

    Add reset button

--- src/components/buy-car-form.js
+++ src/components/buy-car-form.js
@@ -25,7 +25,7 @@ export default class BuyCarForm extends Component {
   models = [];
 
   onBrandChange = (brand) => {
-    this.setState({selectedBrand:brand,selectedModel:''});
+    this.setState({selectedBrand:brand,selectedModel:null});
     this.models = this.models = API_Data.models[brand.value].map(brand => ({
       value: brand.toLowerCase(),
       label: brand
@@ -36,12 +36,18 @@ export default class BuyCarForm extends Component {
     this.setState({selectedModel:model});
   } 
 
+  resetSearch = () => {
+    this.setState({selectedBrand:null,selectedModel:null});
+    this.models = [];
+  }
+
   render() {
     const {selectedBrand, selectedModel, keywords} = this.state;
     return (
       <div className="card BuyCarForm">
         <div className="card-header">
           <strong>Buy a car</strong>
+          <button class="btn btn-sm btn-info float-right" onClick={this.resetSearch}>reset</button>
         </div>
         <div className="card-body">
           <Dropdown label="Brand" 
